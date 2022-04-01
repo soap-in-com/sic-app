72c24168317c95a368719f5070a10621606a5623
2237043ssa <2237043@pcu.ac.kr>
2022-04-01 14:03:02 +0900

    first commit

---
+++ ssa/01-01.c
@@ -0,0 +1,17 @@
+/*
+* 심시아
+* 2237043
+* 서울
+* 24
+*/
+
+#include <stdio.h>
+
+int main(){
+    printf("심시아\n");
+    printf("2237043\n");
+    printf("서울\n");
+    printf("24살\n");
+
+    return 0;
+}
